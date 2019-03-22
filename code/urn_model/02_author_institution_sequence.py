a2da09f35c2799502046e7db06c6676fc636e8f2
ZagHe568 <hezh@vip.qq.com>
2019-03-23 01:26:36 +0800

    first commit

--- code/urn_model/02_author_institution_sequence.py
+++ code/urn_model/02_author_institution_sequence.py
@@ -5,18 +5,19 @@ from utils.pkl_io import open_pkl_file, save_pkl_file
 from utils.entity_io import open_paper
 
 if __name__ == '__main__':
-    authorIds = set()
     paperId_date = open_pkl_file(directory_dataset_description, 'paperId_date')
-    institution_sequence = []
+    authorId_sequence = []
+    affId_sequence = []
 
     for paperId in paperId_date:
         paper = open_paper(paperId)
         for author in paper.authors:
             authorId = author.authorId
             affId = author.affId
-            if authorId in authorIds:
+            if authorId in authorId_sequence:
                 continue
-            authorIds.add(authorId)
-            institution_sequence.append(affId)
+            authorId_sequence.append(authorId)
+            affId_sequence.append(affId)
 
-    save_pkl_file(directory_urn_model, 'institution_sequence', institution_sequence)
+    save_pkl_file(directory_urn_model, 'affId_sequence', authorId_sequence)
+    save_pkl_file(directory_urn_model, 'affId_sequence', affId_sequence)
