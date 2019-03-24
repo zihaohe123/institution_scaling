1ecdb6a319f9612f7a88eaa4d1d1ea12af1590cf
ZagHe568 <hezh@vip.qq.com>
2019-03-24 20:30:08 +0800

    corrected sequence

---
+++ code/urn_model/01_authorId_sequence&affId_sequence.py
@@ -0,0 +1,38 @@
+"""
+This script creates authorId sequence and affiliation sequence ordered by date.
+"""
+import sys
+sys.path.append('..')
+from utils.directories import *
+from utils.pkl_io import open_pkl_file, save_pkl_file
+import time
+
+if __name__ == '__main__':
+    paperIds = open_pkl_file(directory_dataset_description, 'paperIds')
+    authorId_sequence = []  # (authorId, date)
+    affId_sequence = []  # (affId, date)
+
+    num = 0
+    for filename in os.listdir(directory_mag_data):
+        paper_entities = open_pkl_file(directory_mag_data, filename[0:-4])
+        for paper_entity in paper_entities:
+            num += 1
+            if num % 1000 == 0:
+                print(num, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            paperId = paper_entity['Id']
+            if paperId not in paperIds:
+                continue
+            date = paper_entity['D']
+            authors = paper_entity['AA']
+            for author in authors:
+                authorId = author['AuId']
+                affId = author['AfId']
+                authorId_sequence.append((authorId, date))
+                affId_sequence.append((affId, date))
+
+    authorId_sequence.sort(key=lambda t: t[1])
+    affId_sequence.sort(key=lambda t: t[1])
+
+    save_pkl_file(directory_urn_model, 'ordered_authorId_affId_sequence', authorId_sequence)
+    save_pkl_file(directory_urn_model, 'ordered_affId_affId_sequence', authorId_sequence)
+
--- code/urn_model/02_author_institution_sequence.py
+++ code/urn_model/02_author_institution_sequence.py
@@ -1,49 +1,25 @@
+"""
+This script creates (author, institution) sequence and each (author, institution) can appear only once.
+"""
 import sys
 sys.path.append('..')
 from utils.directories import *
 from utils.pkl_io import open_pkl_file, save_pkl_file
-from utils.entity_io import open_paper
 import time
 
 if __name__ == '__main__':
-    paperIds = open_pkl_file(directory_dataset_description, 'paperIds')
-    authorId_sequence = []
-    affId_sequence = []
+    authorId_sequence = open_pkl_file(directory_urn_model, 'ordered_authorId_sequence')
+    affId_sequence = open_pkl_file(directory_urn_model, 'ordered_affId_sequence')
+    authorId_affId_sequence = []  # (authorId, affId)
 
     num = 0
-    for filename in os.listdir(directory_mag_data):
-        paper_entities = open_pkl_file(directory_mag_data, filename[0:-4])
-        for paper_entity in paper_entities:
-            num += 1
-            if num % 1000 == 0:
-                print(num, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-            paperId = paper_entity['Id']
-            if paperId not in paperIds:
-                continue
-            date = paper_entity['D']
-            authors = paper_entity['AA']
-            for author in authors:
-                authorId = author['AuId']
-                affId = author['AfId']
-                authorId_sequence.append((authorId, date))
-                affId_sequence.append((affId, date))
-
-    authorId_sequence.sort(key=lambda t: t[1])
-    affId_sequence.sort(key=lambda t: t[1])
-
-    new_authorId_sequence = []
-    new_affId_sequence = []
-
-    authorIds = set()
-    affIds = set()
-
     for i in range(len(authorId_sequence)):
+        num += 1
+        if num % 1000 == 0:
+            print(num, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         authorId = authorId_sequence[i][0]
-        if authorId not in authorIds:
-            authorIds.add(authorId)
-        new_authorId_sequence.append(authorId)
         affId = affId_sequence[i][0]
-        new_affId_sequence.append(affId)
+        if (authorId, affId) not in authorId_affId_sequence:
+            authorId_affId_sequence.append((authorId, affId))
 
-    save_pkl_file(directory_urn_model, 'authorId_sequence', new_authorId_sequence)
-    save_pkl_file(directory_urn_model, 'affId_sequence', new_affId_sequence)
+    save_pkl_file(directory_urn_model, 'authorId_affId_sequence', authorId_affId_sequence)
